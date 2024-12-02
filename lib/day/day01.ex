24e9820ff2903b40515b2c5537c3cf1d4cc738b9
Joseph Mate <mate.joseph@gmail.com>
2024-12-02 00:00:58 -0500

    day 1 part 1 complete

--- lib/day/day01.ex
+++ lib/day/day01.ex
@@ -10,9 +10,8 @@ defmodule AdventOfCode.Day01 do
 
     @fallback_to_any true
     def sample_part2_files(value) do
-      day_num = AdventOfCode.DaySolution.day(value)
       %{
-        "day#{value.day}_sample.txt" => "day#{value.day}_sample_part1_result.txt"
+        "day#{value.day}_sample.txt" => "day#{value.day}_sample_part2_result.txt"
       }
     end
   end
@@ -37,7 +36,25 @@ defmodule AdventOfCode.Day01 do
   end
 
   def part2(input) do
-    0
+    {first_list, second_list} = input
+    |> String.split("\n")
+    |> Stream.map(&String.split()/1)
+    |> Stream.map(&List.to_tuple()/1)
+    |> Stream.map(fn {a, b} -> {String.to_integer(a), String.to_integer(b)} end)
+    |> Enum.to_list()
+    |> Enum.unzip()
+
+    freq_table =
+      second_list
+      |> Enum.reduce(%{},
+          fn number, acc -> Map.update(acc, number, 1, &(&1 + 1)) end)
+
+    first_list
+    |> Stream.map(fn number -> number * Map.get(freq_table, number, 0) end)
+    |> Enum.to_list()
+    |> Enum.sum()
+
+
   end
 
 end
