80e176940448a958cd37c67697da3b64bf13e6bc
Joseph Mate <mate.joseph@gmail.com>
2024-12-02 00:24:13 -0500

    day 2 part 1 complete

---
+++ lib/day/day02.ex
@@ -0,0 +1,84 @@
+defmodule AdventOfCode.Day02 do
+  defstruct  day: "02"
+
+  defimpl AdventOfCode.DaySolution do
+    def sample_part1_files(value) do
+      %{
+        "day#{value.day}_sample.txt" => "day#{value.day}_sample_part1_result.txt"
+      }
+    end
+
+    @fallback_to_any true
+    def sample_part2_files(value) do
+      %{
+        "day#{value.day}_sample.txt" => "day#{value.day}_sample_part2_result.txt"
+      }
+    end
+  end
+
+    # |> Stream.each(&IO.inspect/1)
+  def part1(input) do
+    input
+    |> String.split("\n")
+    |> Stream.map(&String.split()/1)
+    |> Stream.each(&IO.inspect/1)
+    |> Stream.map(fn row -> Enum.map(row, &String.to_integer()/1) end)
+    |> Stream.each(&IO.inspect/1)
+    |> Stream.map(
+      fn row ->
+        Enum.zip(row, tl(row))
+        |> Enum.map(fn {a,b} -> a - b end)
+      end)
+    |> Stream.each(&IO.inspect/1)
+    |> Stream.filter(
+      fn row ->
+        is_increasing = hd(row) >= 1
+        Enum.map(row, &(
+          &1 >= -3
+          and &1 <= 3
+          and &1 != 0
+          and (
+            (is_increasing and &1 > 0)
+            or (!is_increasing and &1 < 0)
+            )
+          ))
+        |> IO.inspect()
+        |> Enum.all?(&(&1))
+      end
+    )
+    |> Enum.count()
+  end
+
+  def part2(input) do
+    input
+    |> String.split("\n")
+    |> Stream.map(&String.split()/1)
+    |> Stream.each(&IO.inspect/1)
+    |> Stream.map(fn row -> Enum.map(row, &String.to_integer()/1) end)
+    |> Stream.each(&IO.inspect/1)
+    |> Stream.map(
+      fn row ->
+        Enum.zip(row, tl(row))
+        |> Enum.map(fn {a,b} -> a - b end)
+      end)
+    |> Stream.each(&IO.inspect/1)
+    |> Stream.filter(
+      fn row ->
+        is_increasing = hd(row) >= 1
+        Enum.map(row, &(
+          &1 >= -3
+          and &1 <= 3
+          and &1 != 0
+          and (
+            (is_increasing and &1 > 0)
+            or (!is_increasing and &1 < 0)
+            )
+          ))
+        |> IO.inspect()
+        |> Enum.all?(&(&1))
+      end
+    )
+    |> Enum.count()
+  end
+
+end
