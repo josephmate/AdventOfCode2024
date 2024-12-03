f41787463ea489bb02df7067aa092d64bfc82ade
Joseph Mate <mate.joseph@gmail.com>
2024-12-03 00:22:37 -0500

    day 3 part 1 complete with regex

--- lib/day/day03.ex
+++ lib/day/day03.ex
@@ -8,7 +8,6 @@ defmodule AdventOfCode.Day03 do
       }
     end
 
-    @fallback_to_any true
     def sample_part2_files(value) do
       %{
         "day#{value.day}_sample.txt" => "day#{value.day}_sample_part2_result.txt"
@@ -18,9 +17,16 @@ defmodule AdventOfCode.Day03 do
 
     # |> Stream.each(&IO.inspect/1)
   def part1(input) do
-    input
-    |> String.split("\n")
-    |> Stream.map(&String.split()/1)
+    regex = ~r/mul\((\d+),(\d+)\)/
+    Regex.scan(regex, input)
+    |> IO.inspect()
+    |> Enum.map(fn [a,b,c] ->
+      IO.puts("first " <> a  <> " second " <> b <> " third "  <> c)
+      [a,b,c]
+      end)
+    |> Enum.map(fn [_a, b, c] -> {String.to_integer(b), String.to_integer(c)} end)
+    |> Enum.map(fn {a,b} -> a * b end)
+    |> Enum.sum()
   end
 
   def part2(input) do
