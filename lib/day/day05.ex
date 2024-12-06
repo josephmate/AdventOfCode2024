b66c1e3aa5e5aefbc2e44c15a4794c4f063ab991
Joseph Mate <mate.joseph@gmail.com>
2024-12-05 23:02:46 -0500

    day 5 part 1 complete

--- lib/day/day05.ex
+++ lib/day/day05.ex
@@ -17,10 +17,48 @@ defmodule AdventOfCode.Day05 do
 
     # |> Stream.each(&IO.inspect/1)
   def part1(input) do
-    input
-    |> String.split("\n")
+    [page_order_desc, page_production_desc] = input
+    |> String.split("\n\n")
 
-    0
+
+    page_order_before_to_after = page_order_desc
+    |> String.split("\n")
+    |> Stream.map(&String.split(&1, "|"))
+    |> Stream.map(fn [a,b] -> {String.to_integer(a), String.to_integer(b)} end)
+    |> Enum.to_list()
+    |> IO.inspect()
+    #|> Enum.reduce(%{}, fn {key, value}, acc ->
+    #    Map.update(acc, key, [value], fn existing_values -> [value | existing_values] end)
+    #end)
+    #|> IO.inspect()
+    |> MapSet.new()
+    |> IO.inspect()
+
+
+    page_production_desc
+    |> String.split("\n")
+    |> Stream.map(&String.split(&1, ","))
+    |> Stream.map(fn row -> Enum.map(row, &String.to_integer()/1) end)
+    |> Stream.map(&List.to_tuple(&1))
+    |> Stream.filter(&are_pages_in_right_order?(&1, page_order_before_to_after))
+    |> Stream.map(&get_middle(&1))
+    |> Enum.sum()
+  end
+
+  defp are_pages_in_right_order?(pages, page_order_before_to_after) do
+    for i <- 1..(tuple_size(pages)-1) do
+      for j <- 0..(i-1) do
+        !MapSet.member?(page_order_before_to_after, {elem(pages,i), elem(pages,j)})
+      end
+      |> IO.inspect()
+      |> Enum.all?()
+    end
+    |> Enum.all?()
+  end
+
+  defp get_middle(pages) do
+    n = tuple_size(pages)
+    elem(pages,div(n,2))
   end
 
   def part2(input) do
