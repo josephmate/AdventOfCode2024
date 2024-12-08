2702e9ceec0449fc2cdba2cd3f2ab234366c4722
Joseph Mate <mate.joseph@gmail.com>
2024-12-07 23:56:49 -0500

    day 8 ready to start

---
+++ lib/day/day08.ex
@@ -0,0 +1,32 @@
+defmodule AdventOfCode.Day08 do
+  defstruct  day: "08"
+
+  defimpl AdventOfCode.DaySolution do
+    def sample_part1_files(value) do
+      %{
+        "day#{value.day}_sample.txt" => "day#{value.day}_sample_part1_result.txt"
+      }
+    end
+
+    def sample_part2_files(value) do
+      %{
+        "day#{value.day}_sample.txt" => "day#{value.day}_sample_part2_result.txt"
+      }
+    end
+  end
+
+    # |> Stream.each(&IO.inspect/1)
+  def part1(input) do
+    input
+    |> String.split("\n")
+
+    0
+  end
+  def part2(input) do
+    input
+    |> String.split("\n")
+
+    0
+  end
+
+end
