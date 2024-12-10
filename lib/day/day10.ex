3864263b4aae8f951bcbf4004916af355561ea45
Joseph Mate <mate.joseph@gmail.com>
2024-12-10 00:49:31 -0500

    day 10 part 2 complete
    i accidentally implemented it like this for part 1 as a bug :)

--- lib/day/day10.ex
+++ lib/day/day10.ex
@@ -101,7 +101,61 @@ defmodule AdventOfCode.Day10 do
   end
 
   def part2(input) do
-    0
+    trail_map = input
+    |> String.split("\n")
+    |> Enum.map(&String.to_charlist/1)
+    |> Enum.map(fn row -> row
+                          |> Enum.map(fn c -> c - ?0 end)
+    end)
+    |> Enum.map(&List.to_tuple/1)
+    |> List.to_tuple()
+    |> IO.inspect()
+
+    trail_map
+    |> find_trail_heads()
+    |> Enum.map(&score_trail_head_p2(&1, trail_map))
+    |> IO.inspect()
+    |> Enum.sum()
   end
 
+  def score_trail_head_p2(trail_head, trail_map) do
+    score_trail_head_p2_impl(
+      trail_head,
+      trail_map,
+      tuple_size(trail_map),
+      tuple_size(elem(trail_map,0)),
+      MapSet.new()
+    )
+  end
+
+  def score_trail_head_p2_impl({r, c}, trail_map, num_rows, num_cols, visited) do
+    if MapSet.member?(visited, {r, c}) do
+      0
+    else
+      visited = MapSet.put(visited, {r,c})
+      height = index(trail_map, r, c)
+      if height == 9 do
+        1
+      else
+        [
+          {r + 0, c + 1},
+          {r + 0, c - 1},
+          {r + 1, c + 0},
+          {r - 1, c + 0},
+        ]
+        |> Enum.filter(fn {new_r, new_c} ->
+              new_r >= 0 and new_r < num_rows
+          and new_c >= 0 and new_c < num_cols
+        end)
+        |> Enum.filter(fn {new_r, new_c} ->
+          new_height = index(trail_map, new_r, new_c)
+          height == new_height - 1
+        end)
+        |> Enum.map(fn {new_r, new_c} ->
+          score_trail_head_p2_impl({new_r, new_c}, trail_map, num_rows, num_cols, visited)
+        end)
+        |> Enum.sum()
+      end
+    end
+  end
 end
