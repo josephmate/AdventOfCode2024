9f808d0e3a2431199b81a2ed53d288a0287fd1f8
Joseph Mate <mate.joseph@gmail.com>
2024-12-14 23:59:30 -0500

    setup a bunch

---
+++ lib/day/day21.ex
@@ -0,0 +1,26 @@
+defmodule AdventOfCode.Day21 do
+  defstruct  day: "21"
+
+  defimpl AdventOfCode.DaySolution do
+    def sample_part1_files(value) do
+      %{
+        "day#{value.day}_sample.txt" => "day#{value.day}_sample_part1_result.txt"
+      }
+    end
+
+    def sample_part2_files(value) do
+      %{
+        "day#{value.day}_sample.txt" => "day#{value.day}_sample_part2_result.txt"
+      }
+    end
+  end
+
+  def part1(_input) do
+    0
+  end
+
+  def part2(_input) do
+    0
+  end
+
+end
