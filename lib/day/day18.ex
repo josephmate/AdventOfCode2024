0bae6da64807cf15b5534fbfb6c746e8bc73fe21
Joseph Mate <mate.joseph@gmail.com>
2024-12-18 23:41:19 -0500

    day18 part 1 complete

--- lib/day/day18.ex
+++ lib/day/day18.ex
@@ -16,7 +16,81 @@ defmodule AdventOfCode.Day18 do
   end
 
   def part1(_input) do
+    IO.puts("expected")
+    IO.puts("22")
+    IO.puts("actual")
+    solve_part1(
+      Path.join([File.cwd!(), "inputs", "day18_sample.txt"])
+      |> File.read!(),
+      6,
+      12
+    )
+    |> IO.inspect()
+
+    IO.puts("real")
+    solve_part1(
+      Path.join([File.cwd!(), "inputs", "day18.txt"])
+      |> File.read!(),
+      70,
+      1024
+    )
+    |> IO.inspect()
+
     0
+  end
+
+  def solve_part1(input, max_val, steps) do
+    memory_map = input
+    |> String.split("\n")
+    |> Enum.map(fn line -> String.split(line, ",") end)
+    |> Enum.map(fn [x, y] -> {String.to_integer(x), String.to_integer(y)} end)
+    |> Enum.take(steps)
+    |> Enum.reduce(%{}, fn {x, y}, acc ->
+      Map.put(acc, {x, y}, ?#)
+    end)
+
+    find_shortest_path_len(memory_map, max_val, {0,0}, {max_val, max_val})
+  end
+
+  def find_shortest_path_len(memory_map, max_val, {startx,starty}, {endx, endy}) do
+    queue = :queue.new()
+    queue = :queue.in({startx, starty, 0}, queue)
+    visited = find_shortest_path_len_impl(memory_map, max_val, queue, %{})
+    Map.get(visited, {endx, endy})
+  end
+
+  def find_shortest_path_len_impl(memory_map, max_val, queue, visited) do
+    if :queue.is_empty(queue) do
+      # no moves left
+      visited
+    else
+      {{:value, {x, y, moves}}, queue} = :queue.out(queue)
+      if (x > max_val or x < 0
+        or y > max_val or y < 0)
+        or (Map.has_key?(visited, {x,y}) and moves >= Map.get(visited, {x,y}))
+        or (Map.has_key?(memory_map, {x, y}) and Map.get(memory_map, {x, y}) == ?#) do
+        # not a valid move, drop it
+        find_shortest_path_len_impl(memory_map, max_val, queue, visited)
+      else
+        visited = Map.put(visited, {x,y}, moves)
+        queue = [
+          {x + 0, y + 1, moves + 1},
+          {x + 0, y - 1, moves + 1},
+          {x + 1, y + 0, moves + 1},
+          {x - 1, y + 0, moves + 1},
+        ]
+        #|> Enum.filter(fn {x, y, moves} ->
+        #   (x <= max_val and x >= 0
+        #          or y <= max_val and y >= 0)
+        #         and (!Map.has_key?(visited, {x,y}) or moves < Map.get(visited, {x,y}))
+        #         and (!Map.has_key?(memory_map, {x, y}) or Map.get(memory_map, {x, y}) != ?#)
+        #end)
+        |> Enum.reduce(queue, fn element, acc ->
+          :queue.in(element, acc)
+        end)
+        find_shortest_path_len_impl(memory_map, max_val, queue, visited)
+      end
+    end
   end
 
   def part2(_input) do
