42641e5f0967a1d992fb1e18115af94599dbb164
Joseph Mate <mate.joseph@gmail.com>
2024-12-21 17:28:40 -0500

    stuck in an infinite loop somewhere

--- lib/day/day20.ex
+++ lib/day/day20.ex
@@ -52,22 +52,74 @@ defmodule AdventOfCode.Day20 do
       Map.put(map, {r,c}, val)
     end)
 
-    start_posn = map
-    |> Enum.filter(fn {_,_,val} -> val == ?S end)
-    |> Enum.map(fn {r,c,_} -> {r,c} end)
-    |> Enum.take(1)
+    [start_posn | _ ] = map
+    |> Enum.filter(fn {{_,_},val} -> val == ?S end)
+    |> Enum.map(fn {{r,c},_} -> {r,c} end)
 
-    end_posn = map
-    |> Enum.filter(fn {_,_,val} -> val == ?S end)
-    |> Enum.map(fn {r,c,_} -> {r,c} end)
-    |> Enum.take(1)
+    [end_posn | _ ] = map
+    |> Enum.filter(fn {{_,_},val} -> val == ?E end)
+    |> Enum.map(fn {{r,c},_} -> {r,c} end)
 
-    map
+    no_cheat_path = map
     |> bfs(start_posn, end_posn)
+    |> IO.inspect()
   end
 
-  def bfs(map, start_posn, end_posn) do
+  def bfs(map, {r,c}, end_posn) do
+    queue = :queue.new()
+    queue = :queue.in({r,c , 0, []}, queue)
+    visited = %{}
+    visited = Map.put(visited, {r,c}, 0)
+    bfs_impl(map, end_posn, queue, visited)
+  end
 
+  def bfs_impl(map, end_posn, queue, visited) do
+    IO.puts("bfs_impl")
+    IO.inspect(end_posn)
+    IO.inspect(queue)
+    IO.inspect(visited)
+    if :queue.is_empty(queue) do
+      # no moves left
+      # expected to read the end
+      nil
+    else
+      {{:value, {r, c, moves, path}}, queue} = :queue.out(queue)
+      if {r, c} == end_posn do
+        [{r,c, moves} | path]
+        |> Enum.reverse()
+      else
+        IO.puts("inspecting move")
+        IO.inspect(!Map.has_key?(map, {r,c}))
+        IO.inspect(<<Map.get(map, {r,c})>>)
+        IO.inspect(moves)
+        IO.inspect(Map.get(map, {r,c}, 99999999999))
+        path = [{r,c, moves} | path]
+        visited = Map.put(visited, {r,c}, moves)
+        moves = [
+          {r + 0, c + 1, moves + 1, path},
+          {r + 0, c - 1, moves + 1, path},
+          {r + 1, c + 0, moves + 1, path},
+          {r - 1, c + 0, moves + 1, path},
+        ]
+        |> Enum.filter(fn {r,c,moves,_} ->
+          Map.has_key?(map, {r,c})
+          and (Map.get(map, {r,c}) == ?. or Map.get(map, {r,c}) == ?S)
+          and moves < Map.get(map, {r,c}, 99999999999)
+        end)
+
+        queue = moves
+        |> Enum.reduce(queue, fn element, acc ->
+          :queue.in(element, acc)
+        end)
+
+        visited = moves
+        |> Enum.reduce(visited, fn {r,c, moves, _}, acc ->
+          Map.put(acc, {r,c}, moves)
+        end)
+
+        bfs_impl(map, end_posn, queue, visited)
+      end
+    end
   end
 
   def two_dim_list_to_coord_tuples(two_dim_list) do
