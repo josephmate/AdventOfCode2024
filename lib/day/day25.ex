6e7802cf21e90425dba2c98e8a9ba45103eb102d
Joseph Mate <mate.joseph@gmail.com>
2024-12-26 16:10:39 -0500

    day 25 part 1 complete

--- lib/day/day25.ex
+++ lib/day/day25.ex
@@ -15,8 +15,60 @@ defmodule AdventOfCode.Day25 do
     end
   end
 
-  def part1(_input) do
-    0
+  def part1(input) do
+    input = input
+    |> String.split("\n\n")
+    |> Enum.map(&String.split(&1, "\n"))
+    |> Enum.map(&parseSchematic/1)
+
+    keys = input
+    |> Enum.filter(fn {type,_} -> type == :key end)
+    |> Enum.map(fn {_, counts} -> counts end)
+
+    locks = input
+    |> Enum.filter(fn {type,_} -> type == :lock end)
+    |> Enum.map(fn {_, counts} -> counts end)
+
+    key_fits_index = keys
+    |> Enum.map(&generate_all_fit_locks/1)
+    |> List.flatten()
+    |> Enum.frequencies()
+
+    locks
+    |> Enum.map(fn lock -> Map.get(key_fits_index, lock, 0) end)
+    |> Enum.sum()
+  end
+
+  def generate_all_fit_locks({k1, k2, k3, k4, k5}) do
+    for l1 <- 0..(5-k1),
+        l2 <- 0..(5-k2),
+        l3 <- 0..(5-k3),
+        l4 <- 0..(5-k4),
+        l5 <- 0..(5-k5)
+        do
+      {l1, l2, l3, l4, l5}
+    end
+
+  end
+
+  def parseSchematic(input) do
+    key_sizes = 0..(5-1)
+    |> Enum.map(fn col ->
+      input
+      |> Enum.count(fn row ->
+        String.at(row, col) == "#"
+      end)
+    end)
+    |> Enum.map(&(&1 - 1))
+    |> List.to_tuple()
+
+    schematic_type = if List.first(input) == "....." do
+      :key
+    else
+      :lock
+    end
+
+    {schematic_type,key_sizes}
   end
 
   def part2(_input) do
