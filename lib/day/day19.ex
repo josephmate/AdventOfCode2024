b9d4959a3def26706a954a401bcd94d3169ee750
Joseph Mate <mate.joseph@gmail.com>
2024-12-31 15:08:35 -0500

    added some samples to help debug

--- lib/day/day19.ex
+++ lib/day/day19.ex
@@ -4,13 +4,14 @@ defmodule AdventOfCode.Day19 do
   defimpl AdventOfCode.DaySolution do
     def sample_part1_files(value) do
       %{
-        "day#{value.day}_sample.txt" => "day#{value.day}_sample_part1_result.txt"
+        "day#{value.day}_sample.txt" => "day#{value.day}_sample_part1_result.txt",
       }
     end
 
     def sample_part2_files(value) do
       %{
-        "day#{value.day}_sample.txt" => "day#{value.day}_sample_part2_result.txt"
+        "day#{value.day}_sample.txt" => "day#{value.day}_sample_part2_result.txt",
+        "day#{value.day}_sample_02.txt" => "day#{value.day}_sample_02_part1_result.txt"
       }
     end
   end
@@ -33,6 +34,39 @@ defmodule AdventOfCode.Day19 do
     end)
     |> Enum.count()
   end
+
+  # 11
+  # r, rr
+  #
+  # 1 r
+  #   r
+  # 2 rr
+  #   r r
+  #   rr
+  # 3 rrr
+  #   r
+  #     r
+  #       r
+  #     rr
+  #   rr
+  #     r
+  # 5 rrrr
+  #   r 1
+  #     r 2
+  #       r 3
+  #         r 4 *
+  #       rr 4 *
+  #     rr 3
+  #       r 4 *
+  #   rr 2
+  #       r 3
+  #         r 4 *
+  #       rr 4 *
+  # rrrrr
+  #   r 1
+  #     r 2
+  #     rr 3
+  #   rr 2
 
   def part2(input) do
     [components, targets] = input
