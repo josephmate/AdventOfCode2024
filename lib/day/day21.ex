66666cf5f3ff47d38884f6d944237882ad2f1ef9
Joseph Mate <mate.joseph@gmail.com>
2025-01-03 00:38:04 -0500

    Just finished a way to generate they numpad paths, but then I realized that a bunch of them might not be possible because the directional pad cannot immediately change to the direction that we want. For example: suppose the path we want is < followed by >. This is not possible because > is not adjacent to < on the direction pad.

--- lib/day/day21.ex
+++ lib/day/day21.ex
@@ -30,22 +30,34 @@ defmodule AdventOfCode.Day21 do
   end
 
   defp solve_p1(num_string) do
-    generate_paths("A" <> num_string)
+    generate_numpad_paths("A" <> num_string)
     |> IO.inspect()
 
-    "<<<"
+    0
   end
 
-  # +---+---+---+
-  # | 7 | 8 | 9 |
-  # +---+---+---+
-  # | 4 | 5 | 6 |
-  # +---+---+---+
-  # | 1 | 2 | 3 |
-  # +---+---+---+
-  #     | 0 | A |
-  #     +---+---+
   def generate_numpad_paths(target_digits) do
+    target_digits = String.to_charlist(target_digits)
+    # 01234
+    # A029A
+    [
+      generate_numpad_paths_between_points(Enum.at(target_digits, 0), Enum.at(target_digits, 1)),
+      generate_numpad_paths_between_points(Enum.at(target_digits, 1), Enum.at(target_digits, 2)),
+      generate_numpad_paths_between_points(Enum.at(target_digits, 2), Enum.at(target_digits, 3)),
+      generate_numpad_paths_between_points(Enum.at(target_digits, 3), Enum.at(target_digits, 4)),
+    ]
+  end
+
+  defp generate_numpad_paths_between_points(src, dst) do
+    # +---+---+---+
+    # | 7 | 8 | 9 |
+    # +---+---+---+
+    # | 4 | 5 | 6 |
+    # +---+---+---+
+    # | 1 | 2 | 3 |
+    # +---+---+---+
+    #     | 0 | A |
+    #     +---+---+
     graph = [
       {?A, ?0},
       {?A, ?3},
@@ -78,19 +90,77 @@ defmodule AdventOfCode.Day21 do
       {?9, ?8},
       {?9, ?6},
     ]
+    |> Enum.group_by(
+      fn {a, _b} -> a end,
+      fn {_a, b} -> b end
+    )
+    # +---+---+---+
+    # | 7 | 8 | 9 |
+    # +---+---+---+
+    # | 4 | 5 | 6 |
+    # +---+---+---+
+    # | 1 | 2 | 3 |
+    # +---+---+---+
+    #     | 0 | A |
+    #     +---+---+
+
+    visited = %{}
+              |> Map.put(src, 0)
+    queue = :queue.new()
+    queue = :queue.in([src], queue)
+    generate_numpad_paths_between_points_impl(graph, dst, queue, visited, [])
+  end
+
+  defp generate_numpad_paths_between_points_impl(graph, dst, queue, visited, acc) do
+    case :queue.out(queue) do
+      {:empty, _} ->
+        acc
+
+      {{:value, [current_posn | rest]}, rest_queue} ->
+        if current_posn == dst do
+          acc = [ Enum.reverse([current_posn | rest]) | acc ]
+          generate_numpad_paths_between_points_impl(graph, dst, rest_queue, visited, acc)
+        else
+          current_len = length(rest) + 1
+          next_steps = graph
+                       |> Map.get(current_posn, [])
+                       |> Enum.filter(fn next_step -> current_len + 1 <= Map.get(visited, next_step, 1000) end)
+
+          next_queue = next_steps
+                       |> Enum.reduce(rest_queue, fn next_step, acc ->
+                         :queue.in( [next_step, current_posn | rest], acc)
+                       end)
+          next_visited = next_steps
+                         |> Enum.reduce(visited, fn next_step, acc ->
+                           Map.put(acc, next_step, current_len + 1)
+                         end)
+          generate_numpad_paths_between_points_impl(graph, dst, next_queue, next_visited, acc)
+        end
+    end
+  end
+
+  defp _generate_direction_pad_moves() do
+    #     +---+---+
+    #     | ^ | A |
+    # +---+---+---+
+    # | < | v | > |
+    # +---+---+---+
+    _graph = [
+      {?^, ?A},
+      {?^, ?v},
+      {?A, ?>},
+      {?A, ?^},
+      {?<, ?v},
+      {?v, ?<},
+      {?v, ?^},
+      {?v, ?>},
+      {?>, ?v},
+      {?>, ?A},
+    ]
     |> Enum.group_by(fn {a, _b} -> a end)
 
-
+    0
   end
-  # +---+---+---+
-  # | 7 | 8 | 9 |
-  # +---+---+---+
-  # | 4 | 5 | 6 |
-  # +---+---+---+
-  # | 1 | 2 | 3 |
-  # +---+---+---+
-  #     | 0 | A |
-  #     +---+---+
 
 
   defp extract_num(numStr) do
